47f54601aa1f0253eb284064e6faa26815c0f677
Marco D'Agostini <madacol10@gmail.com>
2023-06-08 18:23:59 +0100

    expires -> expires_at

--- migrations/1603322655585_create-session.cjs
+++ migrations/1603322655585_create-session.cjs
@@ -3,10 +3,10 @@ exports.up = pgm => {
         CREATE TABLE "sessions" (
             "session_id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
             "user_id" INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
-            "expire" timestamp(6) NOT NULL
+            "expires_at" timestamp(6) NOT NULL
         );
 
-        CREATE INDEX "IDX_session_expire" ON "sessions" ("expire");
+        CREATE INDEX "IDX_session_expires_at" ON "sessions" ("expires_at");
     `
 };
 
