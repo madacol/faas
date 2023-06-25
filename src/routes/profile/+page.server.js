dc0988bcd3ba7f20c7e640e82bcf3291caa1ce41
Marco D'Agostini <madacol10@gmail.com>
2023-06-25 17:31:42 +0100

    Added a profile page that allows to edit the public bio

---
+++ src/routes/profile/+page.server.js
@@ -0,0 +1,31 @@
+import { sql } from "$lib/server/db";
+
+/** @type {import('@sveltejs/kit').Load} */
+export async function load({ locals }) {
+    const user_id = locals.user.user_id;
+    const {rows: [user]} = await sql`
+        SELECT bio
+            FROM users
+            WHERE user_id=${user_id}
+            ;
+    `;
+    return { user };
+}
+
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	default: async ({ request, locals }) => {
+        const user_id = locals.user.user_id;
+        const data = await request.formData();
+        const bio = data.get("bio");
+
+        const {rows: [new_user]} = await sql`
+            UPDATE users
+                SET bio=${bio}
+                WHERE user_id=${user_id}
+                RETURNING user_id
+                ;
+        `;
+    },
+}
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let data;
+    let bio = data.user.bio;
+</script>
+
+<main>
+    <form method="post">
+        <h2>Public Bio</h2>
+        <textarea name="bio" required value={bio}/>
+
+        <button type="submit">Submit</button>
+    </form>
+</main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        margin: 2em;
+    }
+    form {
+        display: flex;
+        flex-grow: 1;
+        flex-direction: column;
+        align-items: stretch;
+        gap: 1rem;
+        padding: 1rem;
+        text-align: start;
+        max-width: 50em;
+    }
+    textarea {
+        font-size: 0.8em;
+        height: 5em;
+    }
+    button {
+        background-color: #4CAF50;
+        color: white;
+    }
+</style>
+
