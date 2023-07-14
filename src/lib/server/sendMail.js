095640c6039f3bae27dd94342d68700c3818a958
Marco D'Agostini <madacol10@gmail.com>
2023-07-15 00:53:12 +0200

    Send email to requestee after successful payment

---
+++ src/lib/server/sendMail.js
@@ -0,0 +1,37 @@
+import { MAIL_PASSSWORD, MAIL_USER } from "$env/static/private";
+import { createTransport } from "nodemailer";
+
+/**
+ * Send mail to user
+ * @param {string} to - the email to send to
+ * @param {string} subject 
+ * @param {string} text 
+ */
+export function sendMail(to, subject, text) {
+    return new Promise((resolve, reject) => {
+
+        let mailTransporter = createTransport({
+            service: 'gmail',
+            auth: {
+                user: MAIL_USER,
+                pass: MAIL_PASSSWORD
+            }
+        });
+
+        let mailDetails = {
+            from: 'notification@friendpals.ca',
+            to,
+            subject,
+            text
+        };
+
+        mailTransporter.sendMail(mailDetails, function(err, data) {
+            if(err) {
+                console.log({err, data});
+                return reject(err);
+            }
+            console.log('Email sent successfully', data);
+            return resolve(data);
+        });
+    })
+}
