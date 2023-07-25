1e72e80ccec8e4dfdd07f00d6d0f3e0e85655e94
Marco D'Agostini <madacol10@gmail.com>
2023-07-25 17:07:47 +0200

    24 hours -> 7 days

--- src/routes/pal_request/[pal_request_id]/+page.svelte
+++ src/routes/pal_request/[pal_request_id]/+page.svelte
@@ -29,7 +29,7 @@
             If {name} also pays to meet you, is because {name} is genuinely interested.
         </p>
         <p>
-            Don't worry, if {name} doesn't make the payment in 24 hours, we'll
+            Don't worry, if {name} doesn't make the payment in 7 days, we'll
             refund you.
         </p>
         <p />
