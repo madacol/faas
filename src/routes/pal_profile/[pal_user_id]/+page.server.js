dbc08d4290c2550a1a12a8df8668da0798945f84
Marco D'Agostini <madacol10@gmail.com>
2023-07-25 16:04:03 +0200

    Fix: Public profile not showing if it didn't have a pal_request

--- src/routes/pal_profile/[pal_user_id]/+page.server.js
+++ src/routes/pal_profile/[pal_user_id]/+page.server.js
@@ -1,11 +1,10 @@
 import { sql } from "$lib/server/db";
 
 
-export async function load({ locals, params }) {
-    const user_id = locals.user.user_id;
+export async function load({ params }) {
     const {rows: [user]} = await sql`
         SELECT 
-        user_id,
+            user_id,
             name,
             lastname,
             email,
@@ -14,27 +13,9 @@ export async function load({ locals, params }) {
             to_char(birthday,'YYYY-MM-DD') as birthday,
             bio
             FROM users
-            JOIN pal_requests ON pal_requests.requestee_id = users.user_id
             WHERE user_id=${params.pal_user_id}
             GROUP BY user_id
             ;
     `;
     return { user };
-}
-
-/** @type {import('./$types').Actions} */
-export const actions = {
-	default: async ({ request, locals }) => {
-        const user_id = locals.user.user_id;
-        const data = await request.formData();
-        const bio = data.get("bio");
-
-        const {rows: [new_user]} = await sql`
-            UPDATE users
-                SET bio=${bio}
-                WHERE user_id=${user_id}
-                RETURNING user_id
-                ;
-        `;
-    },
 }
