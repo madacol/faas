2a89a1caec60f7c8d2993068ee9ab9df40a06321
Marco D'Agostini <madacol10@gmail.com>
2023-08-17 01:01:06 +0200

    Verification page now handles multiple states and links to `/profile`
    - Add new verification states to display appropriate messages
      and actions to users
    - Page now links to `/profile` instead of `/pals`, give little option to users to bypass filling the profile
    - Add a LinkButton component for better navigation and design

--- src/routes/verify/+page.svelte
+++ src/routes/verify/+page.svelte
@@ -1,9 +1,11 @@
 <script>
-    import { goto } from '$app/navigation'
     import { PUBLIC_STRIPE_KEY } from '$env/static/public'
+    import LinkButton from '$lib/components/LinkButton.svelte'
     import PrimaryButton from '$lib/components/PrimaryButton.svelte'
     import { loadStripe } from '@stripe/stripe-js';
     import { onMount } from 'svelte'
+
+    export let data;
 
     // Make sure to call `loadStripe` outside of a component’s render to avoid
     // recreating the `Stripe` object on every render.
@@ -17,12 +19,30 @@
         stripe = await stripePromise;
     })
 
-    let verificationSubmitted = false;
+    let verification_status = data.user.verification_status;
+
 </script>
 
 <main class="box">
 
-    {#if !verificationSubmitted}
+    {#if verification_status === 'pending'}
+        <h1>Verification pending</h1>
+
+        <p>Thank you for submitting your verification. We will send you an email once your verification is complete.</p>
+
+        <p>In the meantime, we invite you to go to your <a href="/profile">Profile Page</a> and add some details and a friendly photo to your profile. This simple gesture can help us understand each other better and spark wonderful conversations.</p>
+
+        <LinkButton href="/profile">
+            Set profile
+        </LinkButton>
+    {:else if verification_status === 'verified'}
+
+        <h1>Verification complete!</h1>
+
+        <LinkButton href="/pals">
+            Find pals
+        </LinkButton>
+    {:else}
         <img src="/verify.png" alt="An animated person verifying"/>
         <div><h2>We need to verify your identity</h2></div>
         <PrimaryButton
@@ -40,20 +60,11 @@
                 if (error) {
                     console.error({error});
                 } else {
-                    verificationSubmitted = true;
+                    verification_status = 'pending';
                 }
             }}
         >
             Verify
-        </PrimaryButton>
-    {:else}
-        <h1>Verification submitted!</h1>
-        <p>Thank you for submitting your verification. We will send you an email once your verification is complete.</p>
-
-        <p>In the meantime, we invite you to go to your <a href="/profile">Profile Page</a> and add some details and a friendly photo to your profile. This simple gesture can help us understand each other better and spark wonderful conversations.</p>
-
-        <PrimaryButton on:click={() => goto('/pals')}>
-            Find your first pal!
         </PrimaryButton>
     {/if}
 
@@ -66,7 +77,7 @@
         display: flex;
         flex-direction: column;
         justify-content: center;
-        align-items: center;
+        align-items: stretch;
         gap: 2em;
         max-width: 30rem;
         margin: auto;
