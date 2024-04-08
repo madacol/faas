e9a62efecf1fb78c5399f353cf91661077495c47
Marco D'Agostini <madacol10@gmail.com>
2024-04-08 18:14:09 +0200

    fix: oops, didn't check for permission to see the page

--- src/routes/logs/+page.server.js
+++ src/routes/logs/+page.server.js
@@ -1,29 +1,32 @@
+import { checkPermissions_MW } from "$lib/server/checkPermissions_MW";
 import { sql } from "$lib/server/db";
 
-export async function load() {
-
-    const {rows: logs} = await sql`
-        SELECT
-            path,
-            user_session->'email' as email,
-            created_at,
-            method,
-            response_status as status,
-            headers->'referer' as referer,
-            headers->'x-vercel-ip-city' as city,
-            headers->'x-vercel-ip-timezone' as timezone,
-            headers->'x-vercel-ip-country' as country,
-            headers->'x-real-ip' as ip,
-            headers->'user-agent' as user_agent,
-            headers,
-            user_session,
-            params,
-            body
-        FROM logs
-        ORDER BY created_at DESC
-        LIMIT 100;
-    `
-    return {
-        logs
+export const load = checkPermissions_MW(
+    'read_users',
+    async () => {
+        const {rows: logs} = await sql`
+            SELECT
+                path,
+                user_session->'email' as email,
+                created_at,
+                method,
+                response_status as status,
+                headers->'referer' as referer,
+                headers->'x-vercel-ip-city' as city,
+                headers->'x-vercel-ip-timezone' as timezone,
+                headers->'x-vercel-ip-country' as country,
+                headers->'x-real-ip' as ip,
+                headers->'user-agent' as user_agent,
+                headers,
+                user_session,
+                params,
+                body
+            FROM logs
+            ORDER BY created_at DESC
+            LIMIT 300;
+        `
+        return {
+            logs
+        }
     }
-}
+)
